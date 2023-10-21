3b00409b8031253817c0b3b51864ef50545e0acf
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-21 16:33:44 -0600

    Created: createUser

---
+++ backend/controllers/userController.js
@@ -0,0 +1,20 @@
+import User from '../models/User.js';
+
+export const createUser = async (req, res) => {
+
+  const { email, name, password } = req.body;
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    return res.status(400).json({ message: 'El correo electrónico ya está registrado.' });
+  }
+
+  try {
+    const newUser = new User({ email, name, password });
+    const user = await newUser.save();
+    return res.status(201).json({ user, message: 'Usuario registrado con éxito.' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Error al registrar el usuario.' });
+  }
+};
