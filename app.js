0aa0c944466aa64d6b350bd0331ccc5d75b0022e
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-20 18:53:11 -0600

    Created: userRoutes

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import connectDB from './config/db.js';
 import { server } from './config/config.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
 connectDB();
 
 // Routing 
-app.get('/', (req, res) => {
-  res.json({message: 'ok'});
-});
+app.use('/api/users', userRouter);
 
 app.listen(server.PORT, () => {
   console.log(`Example app listening on port ${server.PORT}`)
