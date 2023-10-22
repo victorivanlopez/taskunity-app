c748a6e593f444f5d2e59761edc793df86d492ac
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-22 17:09:44 -0600

    Created: /login route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,5 +4,6 @@ import * as userController from '../controllers/userController.js';
 const router = express.Router();
 
 router.post('/', userController.createUser);
+router.post('/login', userController.authUser);
 
 export default router;
