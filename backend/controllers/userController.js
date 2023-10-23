ea4577ed3a44cdfd3075f9f1c6c6467d03253bad
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-22 20:52:43 -0600

    Created: authUser functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from '../models/User.js';
 import { generateUniqueId } from '../helpers/generateUniqueId.js';
+import { generateJWT } from '../helpers/generateJWT.js';
 
 export const createUser = async (req, res) => {
 
@@ -20,3 +21,28 @@ export const createUser = async (req, res) => {
     return res.status(500).json({ message: 'Error al registrar el usuario.' });
   }
 };
+
+export const authUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({ message: 'El correo no está registrado' });
+  }
+
+  if (!user.confirmed) {
+    return res.status(403).json({ message: 'La cuenta no ha sido confirmada' });
+  }
+
+  if (await user.checkpassword(password)) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateJWT(user._id),
+    })
+  } else {
+    return res.status(403).json({ message: 'La contraseña es incorrecta' });
+  }
+}
