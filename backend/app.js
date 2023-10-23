7873ce423b483949a52ccb9d83348d460da1d244
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-23 16:46:54 -0600

    Added: /api/projects routes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import connectDB from './config/db.js';
 import { server } from './config/config.js';
 import userRouter from './routes/userRoutes.js';
+import projectRouter from './routes/projectRoutes.js'
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,7 @@ connectDB();
 
 // Routing 
 app.use('/api/users', userRouter);
+app.use('/api/projects', projectRouter);
 
 app.listen(server.PORT, () => {
   console.log(`Example app listening on port ${server.PORT}`)
