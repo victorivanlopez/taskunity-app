6e57c6f5e8a711d4488fd1e357f410568ff4eb9a
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-23 14:00:47 -0600

    Created: Project model

---
+++ backend/models/Project.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const projectSchema = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  deadline: {
+    type: Date,
+    default: Date.now(),
+  },
+  client: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  collaborators: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+}, {
+  timestamps: true
+});
+
+const Project = mongoose.model('Project', projectSchema);
+
+export default Project;
