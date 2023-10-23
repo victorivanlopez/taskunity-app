4c6c187d13a48f5e4c9f81e2041152361f7094ee
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-23 17:05:44 -0600

    Created: getProjects functionality

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,5 +1,11 @@
 import Project from '../models/Project.js';
 
+export const getProjects = async (req, res) => {
+  const projects = await Project.find().where('creator').equals(req.user._id);
+
+  res.json(projects);
+}
+
 export const createProject = async (req, res) => {
 
   const { name, description, client } = req.body;
@@ -11,6 +17,6 @@ export const createProject = async (req, res) => {
     const project = await newProject.save();
     return res.status(201).json({ message: 'Proyecto creado con éxito.', project });
   } catch (error) {
-    return res.status(500).json({ message: 'Error al crear el proyecto.'});
+    return res.status(500).json({ message: 'Error al crear el proyecto.' });
   }
 }
