e7f112a1e6213223932cbc4fc0a9de395dc3e428
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-25 13:21:28 -0600

    Configured: Tailwind CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <>
-      <h1>TaskUnity</h1>
+      <h1 className="text-3xl font-bold">TaskUnity</h1>
     </>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
