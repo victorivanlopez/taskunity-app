4869fd977932df672a99cc24c1cb7ad182334bf6
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-26 16:34:39 -0600

    Created: Alert component

---
+++ frontend/src/components/Alert.jsx
@@ -0,0 +1,10 @@
+
+export const Alert = ({ alert = {} }) => {
+  return (
+    <div className={`${(alert.type) ? 'border-[#F87171] bg-[#F87171]' : 'border-[#34D399] bg-[#34D399]'} rounded-lg border-l-4 bg-opacity-[15%] py-2 px-4 my-4 shadow-md`}>
+      <p className="font-medium">
+        {alert.message}
+      </p>
+    </div>
+  )
+}
