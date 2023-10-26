31749a8d4ee2248a072b27ecb5bc2e183f820b6b
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-26 12:57:15 -0600

    Finished: ConfirmAccountPage design

--- frontend/src/auth/pages/ConfirmAccountPage.jsx
+++ frontend/src/auth/pages/ConfirmAccountPage.jsx
@@ -1,6 +1,8 @@
 
 export const ConfirmAccountPage = () => {
   return (
-    <div>ConfirmAccountPage</div>
+    <>
+       <h1 className="text-2xl text-center font-bold uppercase">Confirma tu cuenta para <span className="text-[#423F98]">administrar tus proyectos</span></h1>
+    </>
   )
 }
