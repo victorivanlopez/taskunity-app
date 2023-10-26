759fe97e968eb6d75efec13784757cc923aea21f
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-25 18:24:46 -0600

    Finished: AuthLayout

--- frontend/src/auth/layout/AuthLayout.jsx
+++ frontend/src/auth/layout/AuthLayout.jsx
@@ -2,8 +2,18 @@ import { Outlet } from 'react-router-dom';
 
 export const AuthLayout = () => {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <main className='container mx-auto py-20 md:py-40'>
+      <div className='w-full px-4 md:px-0'>
+        <div className='mx-auto max-w-xl overflow-hidden rounded-lg bg-white py-16 px-10 sm:px-12 md:px-16 shadow'>
+          <div className="mb-16 mx-auto max-w-[250px]">
+            <img
+              src="/assets/taskunity-logo.png"
+              alt="Logo TaskUnity"
+            />
+          </div>
+          <Outlet />
+        </div>
+      </div>
+    </main>
   )
 }
