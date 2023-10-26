758d1f90287501220d34ae9b784d0e3189403bd3
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-26 12:55:41 -0600

    Updated: styles

--- frontend/src/auth/layout/AuthLayout.jsx
+++ frontend/src/auth/layout/AuthLayout.jsx
@@ -2,10 +2,10 @@ import { Outlet } from 'react-router-dom';
 
 export const AuthLayout = () => {
   return (
-    <main className='container mx-auto py-20 md:py-40'>
+    <main className='container mx-auto py-20 md:py-30'>
       <div className='w-full px-4 md:px-0'>
-        <div className='mx-auto max-w-xl overflow-hidden rounded-lg bg-white py-16 px-10 sm:px-12 md:px-16 shadow'>
-          <div className="mb-16 mx-auto max-w-[250px]">
+        <div className='mx-auto max-w-xl overflow-hidden rounded-lg bg-white py-12 px-8 sm:px-10 md:px-12 shadow'>
+          <div className="mb-12 mx-auto max-w-[250px]">
             <img
               src="/assets/taskunity-logo.png"
               alt="Logo TaskUnity"
