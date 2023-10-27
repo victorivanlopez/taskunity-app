03e2952b0477b5c8b4abd3365aae4a8c7d2e7030
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-27 12:33:26 -0600

    Changed: messages responses

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,8 +14,8 @@ export const createUser = async (req, res) => {
   try {
     const newUser = new User({ email, name, password });
     newUser.token = generateUniqueId();
-    const user = await newUser.save();
-    return res.status(201).json({ user, message: 'Usuario registrado con éxito.' });
+    await newUser.save();
+    return res.status(201).json({ message: 'Registro realizado con éxito. Por favor, revisa tu email para confirmar la cuenta.' });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Error al registrar el usuario.' });
