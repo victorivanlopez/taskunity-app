7f6d6978e4f1e9a5550b712b0b65a97b4ac8f782
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-27 11:59:29 -0600

    Updated: conditions styles

--- frontend/src/components/Alert.jsx
+++ frontend/src/components/Alert.jsx
@@ -1,9 +1,9 @@
 
 export const Alert = ({ alert = {} }) => {
   return (
-    <div className={`${(alert.type) ? 'border-[#F87171] bg-[#F87171]' : 'border-[#34D399] bg-[#34D399]'} rounded-lg border-l-4 bg-opacity-[15%] py-2 px-4 my-4 shadow-md`}>
+    <div className={`${(alert?.error) ? 'border-[#F87171] bg-[#F87171]' : 'border-[#34D399] bg-[#34D399]'} rounded-lg border-l-4 bg-opacity-[15%] py-2 px-4 my-4 shadow-md`}>
       <p className="font-medium">
-        {alert.message}
+        {alert?.message}
       </p>
     </div>
   )
