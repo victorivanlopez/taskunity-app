ed537189f0dc00294f8031c2889dd4645f7f81e9
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-27 12:31:58 -0600

    Added: front variables

--- backend/config/config.js
+++ backend/config/config.js
@@ -10,6 +10,10 @@ export const server = {
   PORT: process.env.PORT || 3000,
 };
 
+export const front = {
+  URL: process.env.FRONTEND_URL,
+}
+
 export const secret = {
   JWT_SECRET: process.env.JWT_SECRET,
 }
