922dea37ab2d9c8a7f9c7c35ec25e168a9ac956c
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-27 11:42:23 -0600

    Configured: whitelist CORS

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,30 @@
 import express from 'express';
+import cors from 'cors';
 import connectDB from './config/db.js';
-import { server } from './config/config.js';
 import userRouter from './routes/userRoutes.js';
 import projectRouter from './routes/projectRoutes.js';
 import taskRouter from './routes/taskRoutes.js';
+import { server } from './config/config.js';
 
 const app = express();
 app.use(express.json());
 
 connectDB();
+
+// CORS
+const whitelist = ['http://localhost:5173'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 
 // Routing 
 app.use('/api/users', userRouter);
