bc6f095a48dec5e9ef5db3e9fab734b4d03af486
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-28 19:15:19 -0600

    Added: confirmingUser functionality

--- frontend/src/auth/pages/ConfirmAccountPage.jsx
+++ frontend/src/auth/pages/ConfirmAccountPage.jsx
@@ -1,8 +1,48 @@
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { confirmUser } from '../helpers';
+import { Alert } from '../../components/Alert';
 
 export const ConfirmAccountPage = () => {
+
+  const { token } = useParams();
+  const [alert, setAlert] = useState({});
+  const [isConfirmed, setIsConfirmed] = useState(false);
+
+  const confirmingUser = async () => {
+    const response = await confirmUser(token);
+    setAlert(response);
+    setIsConfirmed(true);
+    
+    if (response?.error) {
+      setIsConfirmed(false);
+    }
+  }
+
+  useEffect(() => {
+    confirmingUser();
+  }, [])
+
   return (
     <>
-       <h1 className="text-2xl text-center font-bold uppercase">Confirma tu cuenta para <span className="text-[#423F98]">administrar tus proyectos</span></h1>
+      <h1 className="text-2xl text-center font-bold uppercase">Confirma tu cuenta para <span className="text-[#423F98]">administrar tus proyectos</span></h1>
+
+      {
+        (alert?.message) && <Alert alert={alert} />
+      }
+
+      <div className='mt-5 flex flex-col justify-center items-center'>
+        {
+          (isConfirmed) && (
+            <Link
+              to='/auth/login'
+              className='text-[#423F98] underline font-bold'
+            >
+              Inicia Sesión
+            </Link>
+          )
+        }
+      </div>
     </>
   )
 }
