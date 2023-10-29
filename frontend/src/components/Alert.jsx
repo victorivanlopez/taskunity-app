f5c23b6ca61882d4c344b37e4a01bdb9d43e6e2d
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-28 19:03:20 -0600

    Updated: styles

--- frontend/src/components/Alert.jsx
+++ frontend/src/components/Alert.jsx
@@ -2,7 +2,7 @@
 export const Alert = ({ alert = {} }) => {
   return (
     <div className={`${(alert?.error) ? 'border-[#F87171] bg-[#F87171]' : 'border-[#34D399] bg-[#34D399]'} rounded-lg border-l-4 bg-opacity-[15%] py-2 px-4 my-4 shadow-md`}>
-      <p className="font-medium">
+      <p className="font-medium text-center">
         {alert?.message}
       </p>
     </div>
