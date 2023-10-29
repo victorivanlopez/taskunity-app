cb535be96c403ceeec5848629f7f2208d74748a0
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-29 15:20:14 -0600

    Created: sendEmailPasswordReset functionality

--- frontend/src/auth/pages/ForgotPasswordPage.jsx
+++ frontend/src/auth/pages/ForgotPasswordPage.jsx
@@ -1,11 +1,46 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks';
+import { Alert } from '../../components';
+import { sendEmailPasswordReset } from '../helpers';
+
+const initialForm = {
+  email: '',
+}
 
 export const ForgotPasswordPage = () => {
+
+  const { email, onInputChange, onResetForm } = useForm(initialForm);
+  const [alert, setAlert] = useState({});
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+
+    if (email === '') {
+      return setAlert({
+        message: 'El correo es obligatorio.',
+        error: true
+      });
+    }
+    setAlert({});
+    const response = await sendEmailPasswordReset(email);
+    setAlert(response);
+
+    if (!response?.error) {
+      onResetForm();
+    }
+  }
+
   return (
     <>
       <h1 className="text-2xl text-center font-bold uppercase">Recupera tu contraseña para <span className="text-[#423F98]">administrar tus proyectos</span></h1>
 
-      <form className="my-10">
+      {alert?.message && <Alert alert={alert} />}
+
+      <form
+        className="my-10"
+        onSubmit={onSubmitForm}
+      >
         <div className="mb-5">
           <label
             htmlFor="email"
@@ -15,6 +50,9 @@ export const ForgotPasswordPage = () => {
             required
             id="email"
             type="email"
+            name='email'
+            value={email}
+            onChange={onInputChange}
             className="border-[#E9EDF4] w-full rounded-md border bg-[#FCFDFE] outline-none focus:border-[#B0A6EB] focus-visible:shadow-none py-3 px-5"
           />
         </div>
