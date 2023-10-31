722da5922274e0a70c58594da39270a184f212e3
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-30 23:20:17 -0600

    Created: login and checkingAuth functionality

--- frontend/src/auth/context/AuthProvider.jsx
+++ frontend/src/auth/context/AuthProvider.jsx
@@ -1,10 +1,43 @@
+import { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
+import { getProfileUser } from '../helpers';
 
 export const AuthProvider = ({ children }) => {
 
+  const [auth, setAuth] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const token = localStorage.getItem('token');
+
+  const checkingAuth = async () => {
+
+    if (!token) {
+      return setIsLoading(false);
+    };
+
+    setIsLoading(true);
+    const user = await getProfileUser(token);
+    setAuth(user);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    checkingAuth();
+  }, [token])
+
+
+  const login = (user = {}) => {
+    setAuth(user);
+    localStorage.setItem('token', user.token);
+  }
+
   return (
     <AuthContext.Provider
-      value={{}}
+      value={{
+        login,
+        auth,
+        isLoading
+      }}
     >
       {children}
     </AuthContext.Provider>
