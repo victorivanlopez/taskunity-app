b7ad0f7c0ed44bd5b27c064876ed0488b8941a4d
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-30 23:19:28 -0600

    Added: PublicRoute and PrivateRoute

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,16 +1,28 @@
 import { AuthLayout } from '../auth/layout/AuthLayout';
+import { PrivateRoute } from './PrivateRoute';
 import AuthRoutes from '../auth/routes/AuthRoutes';
+import { PublicRoute } from './PublicRoute';
 
 const AppRouter = [
   {
+    path: '/',
+    element: <h1>TaskUnity Landing</h1>
+  },
+  {
     path: '/auth',
-    element: <AuthLayout />,
+    element: 
+      <PublicRoute>
+        <AuthLayout />
+      </PublicRoute>,
     children: AuthRoutes,
   },
   {
-    path: '/',
-    element: <h1>TaskUnity Landing</h1>
-  }
+    path: '/projects',
+    element: 
+      <PrivateRoute>
+        <h1>Proyectos</h1>
+      </PrivateRoute>
+  },
 ]
 
 export default AppRouter;
