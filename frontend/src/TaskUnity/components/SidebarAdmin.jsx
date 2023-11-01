97409d785996aa94d41b0ac6f4e62b6f8ef979e5
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-01 15:51:40 -0600

    Changed: icon menu

--- frontend/src/TaskUnity/components/SidebarAdmin.jsx
+++ frontend/src/TaskUnity/components/SidebarAdmin.jsx
@@ -14,9 +14,10 @@ export const SidebarAdmin = () => {
         onClick={() => sidebar.current.classList.toggle('-translate-x-full')}
       >
         <span className="sr-only">Mostrar sidebar</span>
-        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-7 h-7">
+          <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
         </svg>
+
       </button>
 
       <aside ref={sidebar} className="fixed top-0 mt-32 md:mt-16 left-0 w-64 h-screen transition-transform -translate-x-full md:translate-x-0 shadow" aria-label="Sidebar">
