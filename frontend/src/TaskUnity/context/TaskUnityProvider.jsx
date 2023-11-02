29acb95d2c2cc96f67dcbe5b20d37747428e0193
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-01 19:31:42 -0600

    Created: createProject functionality

--- frontend/src/TaskUnity/context/TaskUnityProvider.jsx
+++ frontend/src/TaskUnity/context/TaskUnityProvider.jsx
@@ -1,9 +1,27 @@
-import { TaskUnityContext } from "./TaskUnityContext"
+import { useState } from 'react';
+import { TaskUnityContext } from './TaskUnityContext';
+import { createProject } from '../helpers';
 
 export const TaskUnityProvider = ({ children }) => {
+
+  const [alert, setAlert] = useState({});
+  const token = localStorage.getItem('token');
+
+  const showAlert = (alert) => {
+    setAlert(alert);
+  }
+
+  const startCreateProject = async (project) => {
+    return await createProject(project, token);
+  }
+
   return (
     <TaskUnityContext.Provider
-      value={{}}
+      value={{
+        showAlert,
+        alert,
+        startCreateProject,
+      }}
     >
       {children}
     </TaskUnityContext.Provider>
