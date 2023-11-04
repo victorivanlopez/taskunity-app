1b846f6aea53d8987897326f268f1b7646157f5f
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-04 15:09:48 -0600

    Changed: design styles

--- frontend/src/TaskUnity/components/ProjectItem.jsx
+++ frontend/src/TaskUnity/components/ProjectItem.jsx
@@ -1,27 +1,24 @@
 import { Link } from 'react-router-dom';
+import { formatDate } from '../helpers';
 
 export const ProjectItem = ({ project }) => {
 
-  const { name, client, _id } = project;
+  const { name, client, _id, deadline, description } = project;
+
+  const date = deadline.split('T')[0];
 
   return (
-    <tr className="bg-white border-b">
-      <th scope="row" className="px-6 py-4 font-bold text-black uppercase whitespace-nowrap">
-        {name}
-      </th>
-      <td className="px-6 py-4">
-        {client}
-      </td>
-      <td className="px-6 py-4 whitespace-nowrap">
-        <Link
-          to={_id}
-          className="text-base text-[#545454] font-bold underline hover:text-[#423F98] transition-colors hover:bg-opacity-90"
-        >
-          Ver proyecto
-        </Link>
-      </td>
-    </tr>
+    <Link
+      to={_id}
+      className="flex flex-col justify-between px-6 py-4 border border-gray-200 rounded-lg shadow hover:bg-gray-100 transition-colors"
+    >
+      <h5 className="text-2xl font-bold tracking-tight">{name}</h5>
+      <p className="font-bold text-[#423F98]">{client}</p>
+      <p className='py-2'>{description}</p>
 
-
+      <div className='border-t border-gray-200 mt-2 pt-4 text-xs'>
+        <p className='font-bold'>Fecha de entrega: <span className='font-normal'>{formatDate(date)}</span></p>
+      </div>
+    </Link>
   )
 }
--- frontend/src/TaskUnity/components/ProjectsList.jsx
+++ frontend/src/TaskUnity/components/ProjectsList.jsx
@@ -1,32 +1,13 @@
 import { ProjectItem } from './ProjectItem';
 
-//TODO: Añadir fecha de entrega
-
 export const ProjectsList = ({ projects }) => {
   return (
-    <div className=" overflow-x-auto shadow-md sm:rounded-lg">
-      <table className="w-full text-sm text-left text-gray-500">
-        <thead className="text-xs text-white uppercase bg-[#423F98]">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Nombre
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Cliente
-            </th>
-            <th scope="col" className="px-6 py-3">
-
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            projects.map(project => (
-              <ProjectItem key={project._id} project={project} />
-            ))
-          }
-        </tbody>
-      </table>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {
+        projects.map(project => (
+          <ProjectItem key={project._id} project={project} />
+        ))
+      }
     </div>
   )
 }
