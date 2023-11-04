fa46ade691e0024465f93eb4f70a0dab3c4f1ec9
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-03 19:26:55 -0600

    Updated: name heading and styles

--- frontend/src/TaskUnity/pages/ProjectEditPage.jsx
+++ frontend/src/TaskUnity/pages/ProjectEditPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useTaskUnityContext } from '../../hooks';
 import { FormProject, Spinner } from '../components';
 
@@ -24,7 +24,18 @@ export const ProjectEditPage = () => {
 
   return (
     <>
-      <h1 className="text-4xl font-bold text-center">Editar: {project.name}</h1>
+      <Link
+        to={-1}
+        className='text-[#423F98] flex items-center gap-1 mb-4'
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+          <path fillRule="evenodd" d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clipRule="evenodd" />
+        </svg>
+
+        <span>Regresar</span>
+      </Link>
+
+      <h1 className="text-4xl font-bold text-center">Editando: {project.name}</h1>
 
       <div className="my-10">
         <FormProject btnText="Guardar cambios" initialForm={initialForm} />
