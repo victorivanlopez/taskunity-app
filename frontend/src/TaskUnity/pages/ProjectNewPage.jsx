ff103b85c7c4f52c730973a23f98e9865cec9cbf
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-03 19:18:55 -0600

    Changed: name heading

--- frontend/src/TaskUnity/pages/ProjectNewPage.jsx
+++ frontend/src/TaskUnity/pages/ProjectNewPage.jsx
@@ -12,7 +12,7 @@ export const ProjectNewPage = () => {
 
   return (
     <>
-      <h1 className="text-4xl font-bold text-center">Nuevo proyecto</h1>
+      <h1 className="text-4xl font-bold text-center">Crea un nuevo proyecto</h1>
 
       <div className="my-10">
         <FormProject btnText="Crear proyecto" initialForm={initialForm} />
