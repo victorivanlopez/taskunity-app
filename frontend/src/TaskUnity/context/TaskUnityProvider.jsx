0d4a411dfe801a49dce31e592986398bcc5a0fd7
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-03 18:58:02 -0600

    Created: detele and update project functionality

--- frontend/src/TaskUnity/context/TaskUnityProvider.jsx
+++ frontend/src/TaskUnity/context/TaskUnityProvider.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { TaskUnityContext } from './TaskUnityContext';
-import { createProject, getProjects } from '../helpers';
+import { createProject, deleteProject, getProject, getProjects, updateProject } from '../helpers';
 
 export const TaskUnityProvider = ({ children }) => {
 
   const [projects, setProjects] = useState([]);
+  const [project, setProject] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [alert, setAlert] = useState({});
 
@@ -15,14 +16,40 @@ export const TaskUnityProvider = ({ children }) => {
   const startGetProjects = async () => {
     setIsLoading(true);
     const token = localStorage.getItem('token');
+    if (!token) return;
     const projects = await getProjects(token);
     setIsLoading(false);
     setProjects(projects);
   }
 
-  const startCreateProject = async (project) => {
+  const startSaveProject = async (project) => {
     const token = localStorage.getItem('token');
+    if (!token) return;
+
+    if(project.id) {
+      return await updateProject(project, token);
+    }
     return await createProject(project, token);
+  }
+
+  const startGetProject = async (id) => {
+    setIsLoading(true);
+    const token = localStorage.getItem('token');
+    if (!token) return;
+
+    const project = await getProject(id, token);
+    setIsLoading(false);
+    setProject(project);
+  }
+
+  const startDeleteProject = async (id) => {
+    if(confirm('¿Desea eliminar este proyecto?')) {
+      const token = localStorage.getItem('token');
+      if (!token) return;
+     return await deleteProject(id, token);
+    } else {
+      return null;
+    }
   }
 
   return (
@@ -30,10 +57,13 @@ export const TaskUnityProvider = ({ children }) => {
       value={{
         showAlert,
         alert,
-        startCreateProject,
+        startSaveProject,
         startGetProjects,
         projects,
         isLoading,
+        startGetProject,
+        project,
+        startDeleteProject,
       }}
     >
       {children}
