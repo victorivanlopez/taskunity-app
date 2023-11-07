461e1635d1466d8707be966dd56eb81af5dfa146
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-06 18:28:33 -0600

    Created: ModalFormTask component

--- frontend/src/TaskUnity/components/index.js
+++ frontend/src/TaskUnity/components/index.js
@@ -1,5 +1,7 @@
 export * from './FormProject';
+export * from './FormTask';
 export * from './HeaderAdmin';
+export * from './ModalFormTask';
 export * from './ProjectItem';
 export * from './ProjectsList';
 export * from './SidebarAdmin';
