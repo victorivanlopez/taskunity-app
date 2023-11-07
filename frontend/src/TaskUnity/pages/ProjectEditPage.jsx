cded520ffc5b5d2c8bc9831898969ac5e62354e1
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-07 00:19:48 -0600

    Updated: styles

--- frontend/src/TaskUnity/pages/ProjectEditPage.jsx
+++ frontend/src/TaskUnity/pages/ProjectEditPage.jsx
@@ -24,16 +24,18 @@ export const ProjectEditPage = () => {
 
   return (
     <>
-      <Link
-        to={-1}
-        className='text-[#423F98] flex items-center gap-1 mb-4'
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-          <path fillRule="evenodd" d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clipRule="evenodd" />
-        </svg>
+      <div className='inline-block'>
+        <Link
+          to={-1}
+          className='text-[#423F98] flex items-center gap-1 mb-4'
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+            <path fillRule="evenodd" d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clipRule="evenodd" />
+          </svg>
 
-        <span>Regresar</span>
-      </Link>
+          <span>Regresar</span>
+        </Link>
+      </div>
 
       <h1 className="text-4xl font-bold text-center">Editando: {project.name}</h1>
 
--- frontend/src/TaskUnity/pages/ProjectPage.jsx
+++ frontend/src/TaskUnity/pages/ProjectPage.jsx
@@ -18,16 +18,18 @@ export const ProjectPage = () => {
 
   return (
     <>
-      <Link
-        to='/projects'
-        className='text-[#423F98] flex items-center gap-1 mb-4'
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-          <path fillRule="evenodd" d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clipRule="evenodd" />
-        </svg>
+      <div className='inline-block'>
+        <Link
+          to='/projects'
+          className='text-[#423F98] flex items-center gap-1 mb-4'
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+            <path fillRule="evenodd" d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clipRule="evenodd" />
+          </svg>
 
-        <span>Todos los proyectos</span>
-      </Link>
+          <span>Todos los proyectos</span>
+        </Link>
+      </div>
 
 
       <div className='md:flex md:justify-between items-center'>
