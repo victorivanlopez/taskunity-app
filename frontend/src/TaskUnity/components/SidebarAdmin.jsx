a4213e458969a41df26e18c41418a15e77cb0732
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-07 00:20:46 -0600

    Changed: structure navigation

--- frontend/src/TaskUnity/components/SidebarAdmin.jsx
+++ frontend/src/TaskUnity/components/SidebarAdmin.jsx
@@ -4,7 +4,6 @@ import { NavLink } from 'react-router-dom';
 export const SidebarAdmin = () => {
 
   const sidebar = useRef();
-  const dropdown = useRef();
 
   return (
     <>
@@ -13,7 +12,7 @@ export const SidebarAdmin = () => {
         className="absolute top-4 z-50 p-2 mt-2 ml-3 text-sm text-[#423F98] rounded-lg sm:hidden focus:outline-none focus:ring-2 focus:ring-[#EAF1F7]"
         onClick={() => sidebar.current.classList.toggle('-translate-x-full')}
       >
-        <span className="sr-only">Mostrar sidebar</span>
+        <span className="sr-only">Mostrar menú</span>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-7 h-7">
           <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
         </svg>
@@ -37,40 +36,16 @@ export const SidebarAdmin = () => {
               </NavLink>
             </li>
             <li>
-              <button
-                type="button"
-                className="flex items-center gap-2 p-2 rounded-lg hover:bg-[#423F98] text-[#545454] hover:text-white transition-colors w-full"
-                onClick={() => dropdown.current.classList.toggle('hidden')}
+              <NavLink
+                className={({ isActive }) => `${isActive ? 'bg-[#423F98] text-white' : ''} flex items-center gap-2 p-2 rounded-lg hover:bg-[#423F98] text-[#545454] hover:text-white transition-colors w-full`}
+                to="/projects"
+                onClick={() => sidebar.current.classList.toggle('-translate-x-full')}
               >
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                   <path d="M19.5 21a3 3 0 003-3v-4.5a3 3 0 00-3-3h-15a3 3 0 00-3 3V18a3 3 0 003 3h15zM1.5 10.146V6a3 3 0 013-3h5.379a2.25 2.25 0 011.59.659l2.122 2.121c.14.141.331.22.53.22H19.5a3 3 0 013 3v1.146A4.483 4.483 0 0019.5 9h-15a4.483 4.483 0 00-3 1.146z" />
                 </svg>
-                <span className="flex-1 text-left whitespace-nowrap">Proyectos</span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                </svg>
-              </button>
-              <ul ref={dropdown} className="hidden py-2 space-y-2">
-                <li>
-                  <NavLink
-                    className={({ isActive }) => `${isActive ? 'bg-[#423F98] text-white' : ''} flex items-center w-full p-2 text-[#545454] hover:text-white transition duration-75 rounded-lg pl-11 group hover:bg-[#423F98]`}
-                    to="/projects"
-                    end
-                    onClick={() => sidebar.current.classList.toggle('-translate-x-full')}
-                  >
-                    Todos los proyectos
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    className={({ isActive }) => `${isActive ? 'bg-[#423F98] text-white' : ''} flex items-center w-full p-2 text-[#545454] hover:text-white transition duration-75 rounded-lg pl-11 group hover:bg-[#423F98]`}
-                    to="/projects/new"
-                    onClick={() => sidebar.current.classList.toggle('-translate-x-full')}
-                  >
-                    Nuevo proyecto
-                  </NavLink>
-                </li>
-              </ul>
+                <span>Proyectos</span>
+              </NavLink>
             </li>
           </ul>
 
