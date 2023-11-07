0610e6a79a1c9d91bdcff4fa659f350cee4a46ab
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-06 18:29:53 -0600

    Added: <ModalFormTask />

---
+++ frontend/src/TaskUnity/components/FormTask.jsx
@@ -0,0 +1,120 @@
+import { useParams } from 'react-router-dom';
+import { useForm, useTaskUnityContext } from '../../hooks';
+import { Alert } from '../../components';
+
+const initialForm = {
+  name: '',
+  description: '',
+  dueDate: '',
+  priority: '',
+}
+
+export const FormTask = () => {
+
+  const { name, description, dueDate, priority, onInputChange, formState, onResetForm } = useForm(initialForm);
+  const { startSaveTask, showAlert, alert, project, onShowModalTask } = useTaskUnityContext();
+
+  const { id } = useParams();
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+
+    if ([name, description, dueDate, priority].includes('')) {
+      return showAlert({
+        message: 'Todos los campos son obligatorios.',
+        error: true
+      });
+    }
+    showAlert({});
+    await startSaveTask({ ...formState, project: id });
+    onResetForm();
+    onShowModalTask();
+  }
+
+  return (
+    <>
+      {alert?.message && <Alert alert={alert} />}
+
+      <form
+        className="py-10"
+        onSubmit={onSubmitForm}
+      >
+        <div className="mb-5">
+          <label
+            htmlFor="name"
+            className="font-bold"
+          >Nombre de la tarea</label>
+          <input
+            required
+            id="name"
+            type="text"
+            name='name'
+            value={name}
+            onChange={onInputChange}
+            className="border-[#E9EDF4] w-full rounded-md border bg-[#FCFDFE] outline-none focus:border-[#B0A6EB] focus-visible:shadow-none py-3 px-5"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="description"
+            className="font-bold"
+          >Descripción</label>
+          <textarea
+            required
+            id="description"
+            name='description'
+            value={description}
+            onChange={onInputChange}
+            className="border-[#E9EDF4] w-full rounded-md border bg-[#FCFDFE] outline-none focus:border-[#B0A6EB] focus-visible:shadow-none py-3 px-5"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="dueDate"
+            className="font-bold"
+          >Fecha de entrega</label>
+          <input
+            required
+            id="dueDate"
+            type="date"
+            name='dueDate'
+            max={project.deadline.split('T')[0]}
+            value={dueDate}
+            onChange={onInputChange}
+            className="border-[#E9EDF4] w-full rounded-md border bg-[#FCFDFE] outline-none focus:border-[#B0A6EB] focus-visible:shadow-none py-3 px-5"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="priority"
+            className="font-bold"
+          >Prioridad</label>
+          <select
+            required
+            id="priority"
+            name='priority'
+            value={priority}
+            onChange={onInputChange}
+            className="border-[#E9EDF4] w-full rounded-md border bg-[#FCFDFE] outline-none focus:border-[#B0A6EB] focus-visible:shadow-none py-3 px-5"
+          >
+            <option value="">-- Seleccionar --</option>
+            <option value="baja">Baja</option>
+            <option value="media">Media</option>
+            <option value="alta">Alta</option>
+          </select>
+        </div>
+
+        <div>
+          <button
+            className="w-full cursor-pointer rounded-md border bg-[#423F98] py-3 px-5 text-base text-white font-bold transition-colors hover:bg-opacity-90"
+            type="submit"
+          >
+            Crear tarea
+          </button>
+        </div>
+      </form>
+    </>
+  )
+}
--- frontend/src/TaskUnity/pages/ProjectPage.jsx
+++ frontend/src/TaskUnity/pages/ProjectPage.jsx
@@ -1,13 +1,12 @@
+import { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useTaskUnityContext } from '../../hooks';
-import { useEffect } from 'react';
-import { Spinner } from '../components';
-import { Alert } from '../../components';
+import { ModalFormTask, Spinner } from '../components';
 
 export const ProjectPage = () => {
 
   const { id } = useParams();
-  const { startGetProject, project, isLoading, alert } = useTaskUnityContext();
+  const { startGetProject, project, isLoading, onShowModalTask } = useTaskUnityContext();
 
   useEffect(() => {
     startGetProject(id);
@@ -35,11 +34,21 @@ export const ProjectPage = () => {
         <h1 className='text-4xl font-bold'>{name}</h1>
       </div>
 
-      {alert?.message && <Alert alert={alert} />}
-
       <div className='my-10 space-y-2'>
-        <h2 className='text-2xl font-bold'>Tareas</h2>
+        <button
+          type='button'
+          className='flex gap-2 w-max rounded-lg border bg-[#423F98] px-4 py-2 text-base text-white font-bold transition-colors hover:bg-opacity-90 mt-4 md:mt-0'
+          onClick={onShowModalTask}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z" clipRule="evenodd" />
+          </svg>
+
+          <span>Nueva tarea</span>
+        </button>
       </div>
+
+      <ModalFormTask />
     </>
   )
 }
