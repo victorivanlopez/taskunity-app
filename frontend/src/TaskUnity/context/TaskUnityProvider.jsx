a5c9f27758b38f3fc64c80132caff401a55bfbc4
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-09 14:52:02 -0600

    Created: updateTask functionality

--- frontend/src/TaskUnity/context/TaskUnityProvider.jsx
+++ frontend/src/TaskUnity/context/TaskUnityProvider.jsx
@@ -6,13 +6,15 @@ import {
   deleteProject,
   getProject,
   getProjects,
-  updateProject
+  updateProject,
+  updateTask
 } from '../helpers';
 
 export const TaskUnityProvider = ({ children }) => {
 
   const [projects, setProjects] = useState([]);
   const [project, setProject] = useState({});
+  const [task, setTask] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [alert, setAlert] = useState({});
   const [isOpenModalTask, setIsOpenModalTask] = useState(false);
@@ -64,8 +66,16 @@ export const TaskUnityProvider = ({ children }) => {
   const startSaveTask = async (task) => {
     const token = localStorage.getItem('token');
     if (!token) return;
+
+    const projectUpdated = { ...project };
+
+    if (task.id) {
+      const response = await updateTask(task, token);
+      projectUpdated.tasks = projectUpdated.tasks.map(task => (task._id === response._id) ? response : task);
+      setProject(projectUpdated);
+      return response;
+    }
     const response = await createTask(task, token);
-    const projectUpdated = { ...project.tasks };
     projectUpdated.tasks = [...project.tasks, response.task];
     setProject(projectUpdated);
     return response;
@@ -73,7 +83,13 @@ export const TaskUnityProvider = ({ children }) => {
 
   const onShowModalTask = () => {
     showAlert({});
+    setTask({});
     setIsOpenModalTask(!isOpenModalTask);
+  }
+
+  const onModalEditingTask = (task) => {
+    setTask(task);
+    setIsOpenModalTask(true);
   }
 
   return (
@@ -91,6 +107,8 @@ export const TaskUnityProvider = ({ children }) => {
         startSaveTask,
         isOpenModalTask,
         onShowModalTask,
+        onModalEditingTask,
+        task,
       }}
     >
       {children}
