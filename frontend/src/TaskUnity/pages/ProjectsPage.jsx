c8b66b734e338648c0acfb01c1a0570c3f73de3b
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-12 19:42:12 -0600

    Added: <FormProject />

--- frontend/src/TaskUnity/pages/ProjectsPage.jsx
+++ frontend/src/TaskUnity/pages/ProjectsPage.jsx
@@ -1,11 +1,10 @@
 import { useEffect } from 'react';
 import { useTaskUnityContext } from '../../hooks';
-import { ProjectsList, Spinner } from '../components';
-import { Link } from 'react-router-dom';
+import { AlertDeleteProject, FormProject, Modal, ModalAlert, ProjectsList, Spinner } from '../components';
 
 export const ProjectsPage = () => {
 
-  const { startGetProjects, projects, isLoading } = useTaskUnityContext();
+  const { startGetProjects, projects, onShowModal, isLoading } = useTaskUnityContext();
 
   useEffect(() => {
     startGetProjects();
@@ -17,17 +16,17 @@ export const ProjectsPage = () => {
     <>
       <div className='md:flex md:justify-between'>
         <h1 className='text-4xl font-bold'>Proyectos</h1>
-        <Link
-          to="/projects/new"
+        <button
+          type='button'
           className='flex gap-2 w-max rounded-lg border bg-[#423F98] px-4 py-2 text-base text-white font-bold transition-colors hover:bg-opacity-90 mt-4 md:mt-0'
+          onClick={onShowModal}
         >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
             <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z" clipRule="evenodd" />
           </svg>
 
           <span>Nuevo Proyecto</span>
-        </Link>
-
+        </button>
       </div>
 
       <div className='my-10'>
@@ -37,6 +36,14 @@ export const ProjectsPage = () => {
             : <p>No hay proyectos.</p>
         }
       </div>
+
+      <Modal>
+        <FormProject />
+      </Modal>
+
+      <ModalAlert>
+        <AlertDeleteProject />
+      </ModalAlert>
     </>
   )
 }
