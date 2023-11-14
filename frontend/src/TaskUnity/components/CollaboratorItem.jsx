2c98eae426c8e2d23a0d52a7384df32965264caf
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-14 01:43:24 -0600

    Created: onAddCollaborator functionality

--- frontend/src/TaskUnity/components/CollaboratorItem.jsx
+++ frontend/src/TaskUnity/components/CollaboratorItem.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import { useTaskUnityContext } from '../../hooks';
 
-export const CollaboratorItem = ({collaborator}) => {
+export const CollaboratorItem = ({ collaborator }) => {
 
-  const {_id, name, email} = collaborator;
+  const { _id, name, email } = collaborator;
+
+  const [isDisabled, setIsDisabled] = useState(false);
+  const { startAddCollaborator } = useTaskUnityContext();
+
+  const onAddCollaborator = async ({ target }) => {
+
+    const response = await startAddCollaborator(email);
+
+    if (response?.error) {
+      return setIsDisabled(true);
+    }
+
+    target.textContent = 'Agregado';
+    setIsDisabled(true);
+  }
 
   return (
     <div className="flex justify-between items-center border-t py-4">
@@ -9,8 +26,10 @@ export const CollaboratorItem = ({collaborator}) => {
       <button
         type='button'
         className='flex gap-2 w-max rounded-lg border bg-[#423F98] px-4 py-2 text-base text-white font-bold transition-colors hover:bg-opacity-90 mt-4 md:mt-0 disabled:bg-opacity-50'
+        disabled={isDisabled}
+        onClick={onAddCollaborator}
       >
-        <span>Invitar</span>
+        <span>Agregar</span>
       </button>
     </div>
   )
