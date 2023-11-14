2b4f013480e446fe30aa18b576077e039e27ff11
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-14 13:42:03 -0600

    Created: Modal Alert type functionality

--- frontend/src/TaskUnity/components/AlertDeleteTask.jsx
+++ frontend/src/TaskUnity/components/AlertDeleteTask.jsx
@@ -3,7 +3,7 @@ import { useTaskUnityContext } from '../../hooks';
 
 export const AlertDeleteTask = () => {
 
-  const { onShowModalAlert, startDeleteTask } = useTaskUnityContext();
+  const { onShowModalAlert, startDeleteTask, typeModal } = useTaskUnityContext();
 
   return (
     <>
@@ -20,7 +20,7 @@ export const AlertDeleteTask = () => {
           <button
             type='button'
             className='rounded-lg border border-[#423F98] px-4 py-2 text-base text-[#423F98] font-bold'
-            onClick={onShowModalAlert}
+            onClick={() => onShowModalAlert(typeModal)}
           >
             Cancelar
           </button>
