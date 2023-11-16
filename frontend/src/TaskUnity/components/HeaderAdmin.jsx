3ef7af90c0647edfe7f40230b99958c22d1918db
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-16 17:09:36 -0600

    Added: <SearchProject />

--- frontend/src/TaskUnity/components/HeaderAdmin.jsx
+++ frontend/src/TaskUnity/components/HeaderAdmin.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../hooks';
+import { SearchProject } from './SearchProject';
 
 export const HeaderAdmin = () => {
 
@@ -13,17 +14,9 @@ export const HeaderAdmin = () => {
             <img src="/assets/taskunity-logo.png" className="h-10" alt="TaskUnity Logo" />
           </Link>
 
-          <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-              </svg>
-              <span className="sr-only">Icono buscar</span>
-            </div>
-            <input type="text" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 outline-none focus:border-[#B0A6EB]" placeholder="Buscar..." />
-          </div>
+          <SearchProject />
 
-          <p className="hidden md:block">Bienvenido, <span className="font-bold">{auth.user.name}</span></p>
+          <p className="hidden md:block">Bienvenido, <span className="font-bold">{auth?.user?.name}</span></p>
         </div>
       </nav>
     </header>
