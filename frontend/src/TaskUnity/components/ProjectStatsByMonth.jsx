b3611b22e802639e2e5e57ab9aac0224a00d001c
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-23 19:41:51 -0600

    Created: ProjectStatsByMonth component

---
+++ frontend/src/TaskUnity/components/ProjectStatsByMonth.jsx
@@ -0,0 +1,55 @@
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { useTaskUnityContext } from '../../hooks';
+import { getMonthName } from '../helpers';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend,
+);
+
+export const ProjectStatsByMonth = () => {
+
+  const { projects } = useTaskUnityContext();
+
+  const projectsByMonthYear = projects.reduce((acc, project) => {
+    const createdAt = new Date(project.createdAt);
+    const year = createdAt.getFullYear();
+    const month = createdAt.getMonth() + 1;
+
+    const key = `${getMonthName(month)} ${year}`;
+
+    if (acc[key]) {
+      acc[key]++;
+    } else {
+      acc[key] = 1;
+    }
+
+    return acc;
+  }, {});
+
+  const data = {
+    labels: Object.keys(projectsByMonthYear).map(key => key),
+    datasets: [
+      {
+        label: 'Proyectos por mes',
+        backgroundColor: '#B0A6EB',
+        data: Object.values(projectsByMonthYear),
+      },
+    ],
+  };
+
+  return (
+    <Bar data={data} />
+  )
+}
