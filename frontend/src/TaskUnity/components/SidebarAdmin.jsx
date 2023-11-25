b28d364793cb02e5d939c33a78eab76796352f37
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-24 21:03:40 -0600

    Added: icons component

--- frontend/src/TaskUnity/components/SidebarAdmin.jsx
+++ frontend/src/TaskUnity/components/SidebarAdmin.jsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useTaskUnityContext, useAuth } from '../../hooks/';
+import { BarsIcon, ChartIcon, LogoutIcon, ProjectIcon } from './icons';
 
 export const SidebarAdmin = () => {
 
@@ -32,10 +33,8 @@ export const SidebarAdmin = () => {
         className="absolute top-4 z-50 p-2 mt-2 ml-3 text-sm text-[#423F98] rounded-lg sm:hidden focus:outline-none focus:ring-2 focus:ring-[#EAF1F7]"
         onClick={showSidebar}
       >
+        <BarsIcon size='7' />
         <span className="sr-only">Mostrar menú</span>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-7 h-7">
-          <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
-        </svg>
 
       </button>
 
@@ -48,10 +47,7 @@ export const SidebarAdmin = () => {
                 to="/dashboard"
                 onClick={hiddenSidebar}
               >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                  <path fillRule="evenodd" d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z" clipRule="evenodd" />
-                  <path fillRule="evenodd" d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z" clipRule="evenodd" />
-                </svg>
+                <ChartIcon />
                 <span>Dashboard</span>
               </NavLink>
             </li>
@@ -61,9 +57,7 @@ export const SidebarAdmin = () => {
                 to="/projects"
                 onClick={hiddenSidebar}
               >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                  <path d="M19.5 21a3 3 0 003-3v-4.5a3 3 0 00-3-3h-15a3 3 0 00-3 3V18a3 3 0 003 3h15zM1.5 10.146V6a3 3 0 013-3h5.379a2.25 2.25 0 011.59.659l2.122 2.121c.14.141.331.22.53.22H19.5a3 3 0 013 3v1.146A4.483 4.483 0 0019.5 9h-15a4.483 4.483 0 00-3 1.146z" />
-                </svg>
+                <ProjectIcon />
                 <span>Proyectos</span>
               </NavLink>
             </li>
@@ -76,9 +70,7 @@ export const SidebarAdmin = () => {
                 className="flex items-center gap-2 p-2 rounded-lg hover:bg-[#423F98] text-[#545454] hover:text-white transition-colors w-full"
                 onClick={onLogout}
               >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-7 h-7">
-                  <path fillRule="evenodd" d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm10.72 4.72a.75.75 0 011.06 0l3 3a.75.75 0 010 1.06l-3 3a.75.75 0 11-1.06-1.06l1.72-1.72H9a.75.75 0 010-1.5h10.94l-1.72-1.72a.75.75 0 010-1.06z" clipRule="evenodd" />
-                </svg>
+                <LogoutIcon />
                 Cerrar Sesión
               </button>
             </li>
--- frontend/src/TaskUnity/components/TabsProject.jsx
+++ frontend/src/TaskUnity/components/TabsProject.jsx
@@ -1,5 +1,6 @@
 import { Tab } from '@headlessui/react';
 import { TabTasks, TabCollaborators } from './';
+import { TaskIcon, CollaboratorsIcon } from './icons';
 
 export const TabsProject = () => {
 
@@ -11,31 +12,26 @@ export const TabsProject = () => {
         <Tab.List className='flex my-5'>
           <Tab className={({ selected }) =>
             classNames(
-              'flex w-60 justify-center gap-2 py-4 font-medium transition-colors duration-300 mx-1 focus:outline-none',
+              'flex w-60 justify-center gap-2 py-4 font-semibold transition-colors duration-300 mx-1 focus:outline-none',
               selected
                 ? 'border-b-2 border-[#423F98] text-[#423F98]'
-                : 'hover:text-[#423F98] border-b-2 hover:border-[#423F98]'
+                : 'hover:text-[#423F98] border-b-2 hover:border-[#423F98] text-[#545454]'
             )
           }
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75" />
-            </svg>
-
+            <TaskIcon />
             <span>Tareas</span>
           </Tab>
           <Tab className={({ selected }) =>
             classNames(
-              'flex w-60 justify-center gap-2 py-4 font-medium transition-colors duration-300 mx-1 focus:outline-none',
+              'flex w-60 justify-center gap-2 py-4 font-semibold transition-colors duration-300 mx-1 focus:outline-none',
               selected
                 ? 'border-b-2 border-[#423F98] text-[#423F98]'
-                : 'hover:text-[#423F98] border-b-2 hover:border-[#423F98]'
+                : 'hover:text-[#423F98] border-b-2 hover:border-[#423F98] text-[#545454]'
             )
           }
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
-            </svg>
+            <CollaboratorsIcon />
             <span>Colaboradores</span>
           </Tab>
         </Tab.List>
