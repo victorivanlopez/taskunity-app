feb0158d383e83af0aa63e2af4142cb908f69dcb
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-27 12:19:10 -0600

    Updated: Navigate route

--- frontend/src/auth/pages/LoginPage.jsx
+++ frontend/src/auth/pages/LoginPage.jsx
@@ -35,7 +35,7 @@ export const LoginPage = () => {
     }
     setAlert({});
     login(response);
-    navigate('/dashboard', { replace: true });
+    navigate('/projects', { replace: true });
   }
 
   return (
