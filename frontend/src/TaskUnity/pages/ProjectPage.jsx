2ba5ed94db9f4687981513ddddad927adc9c0f86
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-26 19:53:12 -0600

    Added: <ArrowLeftIcon />

--- frontend/src/TaskUnity/pages/ProjectPage.jsx
+++ frontend/src/TaskUnity/pages/ProjectPage.jsx
@@ -14,20 +14,21 @@ import {
   TabsProjectCollaborator
 } from '../components';
 import { Alert } from '../../components';
+import { ArrowLeftIcon } from '../components/icons';
 
 let socket;
 
 export const ProjectPage = () => {
 
   const { id } = useParams();
-  const { 
-    startGetProject, 
-    project, 
-    isLoading, 
-    typeModal, 
-    alert, 
-    addTaskToState, 
-    deteleTaskToState, 
+  const {
+    startGetProject,
+    project,
+    isLoading,
+    typeModal,
+    alert,
+    addTaskToState,
+    deteleTaskToState,
     updateTaskToState,
     toggleTaskToState,
   } = useTaskUnityContext();
@@ -79,12 +80,9 @@ export const ProjectPage = () => {
       <div className='inline-block'>
         <Link
           to='/projects'
-          className='text-[#423F98] flex items-center gap-1 mb-4'
+          className='text-taskunity-800 flex items-center gap-1 mb-4'
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-            <path fillRule="evenodd" d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clipRule="evenodd" />
-          </svg>
-
+          <ArrowLeftIcon />
           <span>Todos los proyectos</span>
         </Link>
       </div>
