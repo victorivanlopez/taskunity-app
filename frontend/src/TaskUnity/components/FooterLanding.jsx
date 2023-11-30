b59a26f38b48a30395d259496f0f8040441f5e73
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-11-30 16:44:52 -0600

    Updated: design

--- frontend/src/TaskUnity/components/FooterLanding.jsx
+++ frontend/src/TaskUnity/components/FooterLanding.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { getCurrentYear } from '../helpers';
 
 export const FooterLanding = () => {
@@ -6,7 +7,12 @@ export const FooterLanding = () => {
       <div className='max-w-6xl mx-auto'>
         <div className='mx-4 md:mx-8 lg:mx-0 flex flex-col md:flex-row gap-4 md:gap-0 justify-between'>
           <p className='text-white font-semibold text-sm text-center md:text-left'>©{getCurrentYear()} TaskUnity. Todos los derechos reservados. Desarrollado por Víctor Iván López</p>
-          <p className='text-white font-semibold text-sm text-center md:text-left'>Aviso de Privacidad</p>
+          <Link
+            to='privacy'
+            className='text-white font-semibold text-sm text-center md:text-left'
+          >
+            Aviso de Privacidad
+          </Link>
         </div>
       </div>
     </footer>
