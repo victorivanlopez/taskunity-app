3b2b1446ef484163638707fe4f98c5e48f7051d4
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-04-13 19:57:03 -0600

    Updated: styles

--- frontend/src/auth/layout/AuthLayout.jsx
+++ frontend/src/auth/layout/AuthLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export const AuthLayout = () => {
   return (
-    <main className='container mx-auto py-20 md:py-30'>
+    <main className='container mx-auto py-10 md:py-30'>
       <div className='w-full px-4 md:px-0'>
         <div className='mx-auto max-w-xl overflow-hidden rounded-lg bg-white py-12 px-4 md:px-12 shadow'>
           <div className="mb-12 mx-auto max-w-[250px]">
