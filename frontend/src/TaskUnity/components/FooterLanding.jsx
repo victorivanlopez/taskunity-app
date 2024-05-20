2ba2e0b75481ecc3f21659c8d50e8683135ae2a9
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-05-20 17:52:43 -0600

    Added: link to website

--- frontend/src/TaskUnity/components/FooterLanding.jsx
+++ frontend/src/TaskUnity/components/FooterLanding.jsx
@@ -6,7 +6,7 @@ export const FooterLanding = () => {
     <footer className='py-10 bg-taskunity-950'>
       <div className='max-w-6xl mx-auto'>
         <div className='mx-4 md:mx-8 lg:mx-0 flex flex-col md:flex-row gap-4 md:gap-0 justify-between'>
-          <p className='text-white font-semibold text-sm text-center md:text-left'>©{getCurrentYear()} TaskUnity. Todos los derechos reservados. Desarrollado por <span className='font-bold'>victorivanlopez.com</span></p>
+          <p className='text-white font-semibold text-sm text-center md:text-left'>©{getCurrentYear()} TaskUnity. Todos los derechos reservados. Desarrollado por <a href='https://www.victorivanlopez.com'target='_blank' className='font-bold'>victorivanlopez.com</a></p>
           <Link
             to='privacy'
             className='text-white font-semibold text-sm text-center md:text-left'
